4b5efdfe0f30b03eca7e5eaa3125e8ca489691b1
Carl Pi-Cheng Huang <ph9xn@virginia.edu>
2019-03-17 17:10:07 -0500

    replace_all finished, but coalesce is just more abstract and powerful.

--- tests/testthat/test_test.R
+++ tests/testthat/test_test.R
@@ -2,5 +2,5 @@ library(testthat)
 
 expect_that(cpo_hello(), equals("Hello, world of 6 CPO cars!"))
 expect_that(cpo_hello(1), equals("Hello, world of 6 CPO cars!"))
-expect_that(cpo_hello(1000), equals("Hello, world of 6 CPO cars!"))
+expect_that(cpo_hello(NA), equals("Hello, world of 6 CPO cars!"))
 expect_that(cpo_hello('koggi tom'), equals("Hello, world of 6 CPO cars!"))
