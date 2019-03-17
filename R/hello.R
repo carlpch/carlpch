23d4827983044053f98d312d0d74f06613199076
Carl Pi-Cheng Huang <ph9xn@virginia.edu>
2019-03-17 12:32:24 -0500

    Committing my first ever R package function: cpo_hello()

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,18 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+#' Say hello to CPO cars
+#'
+#' This \code{hello()} help us say hello to all the CPO cars!
+#' There were 6 CPO (Certified Pre-owned) cars in a lake,
+#' they did not realize the surrounding water until someone
+#' revealed the fact to them. May be to late to fix them,
+#' there is still time to say hello.
+#' @return You give anything to the function (including nothing) Then, you receive a "Hello" from the function.
+#' @examples
+#' hello()
+#' hello('6 CPO Cars!')
+cpo_hello <- function() {
+  print("Hello, world of 6 CPO cars!")
 }
+
