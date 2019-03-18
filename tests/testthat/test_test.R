9a6826a5bab8e022b0933870f93748324cc3a368
Carl Pi-Cheng Huang <ph9xn@virginia.edu>
2019-03-17 21:39:23 -0500

    remove testing function (cpo_hello), package now fully serious

--- tests/testthat/test_test.R
+++ tests/testthat/test_test.R
@@ -5,14 +5,6 @@ library(testthat)
 library(tibble)
 library(dplyr)
 
-# greetings.R ----------------------------------------------------------
-# cpo_hello()
-expect_that(cpo_hello(), equals("Hello, world of 6 CPO cars!"))
-expect_that(cpo_hello(1), equals("Hello, world of 6 CPO cars!"))
-expect_that(cpo_hello(c(0:100)), equals("Hello, world of 6 CPO cars!"))
-expect_that(cpo_hello(NA), equals("Hello, world of 6 CPO cars!"))
-expect_that(cpo_hello('koggi tom'), equals("Hello, world of 6 CPO cars!"))
-
 # table_operation.R ----------------------------------------------------
 
 # Testing replace_all()
