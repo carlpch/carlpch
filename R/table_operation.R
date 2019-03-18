2b86e5878b1b04da3cc6d308ebbe75870a36d860
Carl Pi-Cheng Huang <ph9xn@virginia.edu>
2019-03-17 19:46:58 -0500

    ready to branch for dev

--- R/table_operation.R
+++ R/table_operation.R
@@ -1,20 +1,20 @@
 #' Replace all x in a tibble with y
 #'
 #' While the \code{dplyr} grammar provides great functionality to
-#' replace any cell value, \code{full_replace} makes code more readable and intuitive.
+#' replace any cell value, \code{replace_all} makes code more readable and intuitive.
 #' @param table        This needs to be a  \code{dplyr}  \code{tibble}.
 #' @param target       The target value to be replaced. This can be number, string, or NA.
 #' @param replacement  The value that will be used to replace the \code{target}.
 #' @return This function will return a \code{tibble} with all the \code{x} replaced by \code{y}.
 #' @examples
 #' data = tibble(a=1:10, b=11:20, c='Test')
-#' full_replace(table = data,target='Test',replacement=100)
+#' replace_all(table = data,target='Test',replacement=100)
 #'
 #' data = tibble(a=1:10, b=11:20, c=NA)
-#' full_replace(table = data,target=NA,replacement=100)
+#' replace_all(table = data,target=NA,replacement=100)
 #' @export
 
-full_replace <- function(table,target,replacement) {
+replace_all <- function(table,target,replacement) {
   if (is.na(target)==TRUE){
     table = mutate_all(table, funs(ifelse(is.na(.),replacement,.)))
   } else {
--- tests/testthat/test_test.R
+++ tests/testthat/test_test.R
@@ -1,6 +1,15 @@
 library(testthat)
+library(tidyverse)
 
+# greetings.R ----------------------------------------------------------
+# cpo_hello()
 expect_that(cpo_hello(), equals("Hello, world of 6 CPO cars!"))
 expect_that(cpo_hello(1), equals("Hello, world of 6 CPO cars!"))
+expect_that(cpo_hello(c(0:100)), equals("Hello, world of 6 CPO cars!"))
 expect_that(cpo_hello(NA), equals("Hello, world of 6 CPO cars!"))
 expect_that(cpo_hello('koggi tom'), equals("Hello, world of 6 CPO cars!"))
+
+# table_operation.R ----------------------------------------------------
+
+# Testing replace_all()
+
